a675869664d9c8bccf5992b71893f0f8b74e5b4a
Jon Hunter <jon@globaloria.com>
2015-06-29 11:56:35 -0400

    Reverting edits to this file that broke orion

--- bundles/org.eclipse.orion.client.ui/web/edit/edit.js
+++ bundles/org.eclipse.orion.client.ui/web/edit/edit.js
@@ -7,31 +7,14 @@
  * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html). 
  *
  * Contributors:
- *     IBM Corporation - initial API and implementation
+ *     IBM Corporation - initial API and implementation
  *******************************************************************************/
 /*eslint-env browser, amd*/
-define(['embeddedEditor/builder/embeddedEditor'],
-function(mEmbeddedEditor) {
-	var contents = 'var foo = "bar";\n' +
-						 "var bar = foo;\n" + 
-						 "/*\n" + 
-						 " * test demo\n" + 
-						 "*/\n" + 
-						 "function test(){\n" + 
-						 "	var foo1 = bar.lastIndexOf(char, from);\n" + 
-						 "}\n" + 
-						"//Keep editting in this demo and try the content assit, probem validations and hover service!\n" +
-						 "var foo2 = foo."; 
-	var contents1 = 
-						 '<div class="embeddedEditorParentOuter" id="embeddedEditor1">\n' + 
-						 "</div>\n" + 
-						 "<span>var foo2</span>"; 
-	var embeddedEditor = new mEmbeddedEditor();
-	embeddedEditor.create({parent: "embeddedEditor"}).then(function(editorViewer) {
-		document.getElementById("progressMessageDiv").textContent = "Plugins loaded!";
-		editorViewer.setContents(contents, "application/javascript");
+define(['orion/browserCompatibility', 'orion/bootstrap', 'edit/setup'], function(mBrowserCompatibility, mBootstrap, mSetup) {
+	mBootstrap.startup().then(function(core) {
+		var serviceRegistry = core.serviceRegistry;
+		var pluginRegistry = core.pluginRegistry;
+		var preferences = core.preferences;
+		mSetup.setUpEditor(serviceRegistry, pluginRegistry, preferences, false);  // not read only
 	});
-	embeddedEditor.create({parent: "embeddedEditor1",
-						   contentType: "text/html",
-						   contents: contents1});
 });
